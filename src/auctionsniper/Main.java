a4e09111d63bb6998ea4b3317d2e273d9a475da0
Gordon McCreight <gordon@mccreight.com>
2015-01-18 09:10:00 -0800

    Consistently passes

--- src/auctionsniper/Main.java
+++ src/auctionsniper/Main.java
@@ -29,9 +29,13 @@ public class Main {
 	}
 
 	public static void main() throws Exception {
-    System.out.println("debug Main main 1");
-    System.out.println("debug Main main 2");
-    System.out.println("debug Main main 3");
+    try {
+        Thread.sleep(500);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+        // handle the exception...        
+        // For example consider calling Thread.currentThread().interrupt(); here.
+    }
 		Main main = new Main();
 	}
 
